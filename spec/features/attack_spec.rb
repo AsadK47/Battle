12eb41873aadadcea51209c5a4554391c4cce488
Asad Khan <asad_khan47@hotmail.co.uk>
2018-11-14 17:56:59 +0000

    User Story 4 - Complete - As Player 1, So I can start to win a game of Battle, I want my attack to reduce Player 2's HP by 10

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,4 +4,11 @@ feature "Attacks" do
     click_button "Attack"
     expect(page).to have_content "Asad attacked Chris!"
   end
+
+  scenario "Player 2 loses 10HP" do
+    sign_in_and_play
+    click_button "Attack"
+    expect(page).not_to have_content "Chris: 60HP"
+    expect(page).to have_content "Chris: 50HP"
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,14 +2,33 @@ require "player"
 
 describe Player do
 
+  let(:name) { double :name }
+  subject(:player_1) { described_class.new("Asad") }
+  subject(:player_2) { described_class.new("Chris") }
+
   describe "#name" do
-    let(:name) { double :name }
-    subject(:player) { described_class.new(name) }
-
     it "Returns the users name" do
-      expect(player.name).to eq name
+      expect(player_1.name).to eq "Asad"
     end
+  end
 
+  describe "hit_points" do
+    it "returns the hit_points" do
+      expect(player_1.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe "attack" do
+    it "damages the player" do
+      expect(player_2).to receive(:receive_damage)
+      player_1.attack(player_2)
+    end
+  end
+
+  describe "#receive_damage" do
+    it "reduces the players hit_points" do
+      expect{ player_2.receive_damage }.to change { player_2.hit_points }.by(-10)
+    end
   end
 
 end
