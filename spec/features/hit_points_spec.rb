bb831f19570f3a0c5c023ddab3ac904e17322fa0
Asad Khan <asad_khan47@hotmail.co.uk>
2018-11-14 15:50:05 +0000

    DRY'd out code using a web_helper.rb file which defines a sign in and play method

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature "hit_points" do
   scenario "can view players HP" do
-    visit '/'
-    fill_in :player_1_name, with: "Asad"
-    fill_in :player_2_name, with: "Chris"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content "Chris: 60HP"
   end
 end
--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -1,9 +1,6 @@
 feature "names" do
   scenario "Player 1 and 2 can enter names" do
-    visit '/'
-    fill_in :player_1_name, with: "Asad"
-    fill_in :player_2_name, with: "Chris"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content "Asad vs Chris"
   end
 end
