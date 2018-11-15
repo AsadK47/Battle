4e1b24e5d674f809a7bdd48abf392874a89a4e0d
Asad Khan <asad_khan47@hotmail.co.uk>
2018-11-15 14:51:28 +0000

    Adding switch - failed unfortunately

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -11,4 +11,11 @@ feature "Attacks" do
     expect(page).not_to have_content "Chris: 60HP"
     expect(page).to have_content "Chris: 50HP"
   end
+
+  scenario "be attacked by player 2 after player 1 attack" do
+    sign_in_and_play
+    click_button "Attack"
+    click_button "Attack"
+    expect(page).to have_content "Chris attacked Asad!"
+  end
 end
