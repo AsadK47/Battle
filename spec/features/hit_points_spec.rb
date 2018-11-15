9109e2ef21e9957b69767bc9ddc97ced9ff507a9
Asad Khan <asad_khan47@hotmail.co.uk>
2018-11-15 11:58:48 +0000

    Added both players HP values

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,6 +1,13 @@
 feature "hit_points" do
-  scenario "can view players HP" do
+
+  scenario "Can view player_1 HP" do
+    sign_in_and_play
+    expect(page).to have_content "Asad: 60HP"
+  end
+
+  scenario "can view player_2 HP" do
     sign_in_and_play
     expect(page).to have_content "Chris: 60HP"
   end
+  
 end
